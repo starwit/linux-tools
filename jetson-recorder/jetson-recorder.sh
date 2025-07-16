f0015422be0180a74577332802f19405ce22aa94
flonix8 <flstanek@googlemail.com>
2025-07-16 13:14:39 +0200

    Hide ffmpeg output; log pids

--- jetson-recorder/jetson-recorder.sh
+++ jetson-recorder/jetson-recorder.sh
@@ -14,7 +14,7 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 mkdir -p "${OUTPUT_PATH}"
 
 # Record video and GPS data with a common timestamp
-ffmpeg -i rtsp://${USERNAME}:${PASSWORD}@${CAMERA_URL} -c copy "${OUTPUT_PATH}/${TIMESTAMP}_video.mkv" &
+ffmpeg -i rtsp://${USERNAME}:${PASSWORD}@${CAMERA_URL} -c copy "${OUTPUT_PATH}/${TIMESTAMP}_video.mkv" 2> "${OUTPUT_PATH}/${TIMESTAMP}_ffmpeg.log" &
 FFMPEG_PID=$!
 
 # Record GPS data from a serial device (e.g., /dev/ttyUSB1), add a timestamp to each line and skip empty lines
@@ -22,6 +22,8 @@ socat /dev/ttyUSB1,raw,echo=0 - | while IFS= read -r line; do
     [ -n "$line" ] && echo "$(date --iso=ns) $line"
 done > "${OUTPUT_PATH}/${TIMESTAMP}_gps.log" &
 GPS_PID=$!
+
+echo "Recording started. FFMPEG PID: $FFMPEG_PID, GPS_PID: $GPS_PID"
 
 # Wait for one of the processes to finish
 wait -n $FFMPEG_PID $GPS_PID
