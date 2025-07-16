32bdbf78db4726e0cee24f9dd4d9ba7f10c6b760
flonix8 <flstanek@googlemail.com>
2025-07-16 13:26:49 +0200

    Delimit timestamp by semicolon; Do not log ffmpeg stats

--- jetson-recorder/jetson-recorder.sh
+++ jetson-recorder/jetson-recorder.sh
@@ -14,12 +14,12 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 mkdir -p "${OUTPUT_PATH}"
 
 # Record video and GPS data with a common timestamp
-ffmpeg -i rtsp://${USERNAME}:${PASSWORD}@${CAMERA_URL} -c copy "${OUTPUT_PATH}/${TIMESTAMP}_video.mkv" 2> "${OUTPUT_PATH}/${TIMESTAMP}_ffmpeg.log" &
+ffmpeg -i rtsp://${USERNAME}:${PASSWORD}@${CAMERA_URL} -c copy -nostats "${OUTPUT_PATH}/${TIMESTAMP}_video.mkv" 2> "${OUTPUT_PATH}/${TIMESTAMP}_ffmpeg.log" &
 FFMPEG_PID=$!
 
 # Record GPS data from a serial device (e.g., /dev/ttyUSB1), add a timestamp to each line and skip empty lines
 socat /dev/ttyUSB1,raw,echo=0 - | while IFS= read -r line; do
-    [ -n "$line" ] && echo "$(date --iso=ns) $line"
+    [ -n "$line" ] && echo "$(date --iso=ns);$line"
 done > "${OUTPUT_PATH}/${TIMESTAMP}_gps.log" &
 GPS_PID=$!
 
